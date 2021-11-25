7211a1a665dce93f473d9be7372eaef93fe4bcd8
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-25 15:49:02 +0500

    added file

---
+++ script.js
@@ -0,0 +1,81 @@
+// Ckecing array or not
+
+// const arr = (input) => {
+//     if (typeof input === typeof []) {
+//         console.log('this is an array')
+//     } else {
+//         console.log('Not array')
+//     }
+// }
+
+// arr([1, 2, 3, 4,]);
+// arr(1235);
+// arr('str');
+
+
+
+// cloning js array
+
+
+// const arrClone = (input) => {
+//     let array = [1, 2, 3, 4];
+//     let arra = array.slice(0);
+//     return arra
+// }
+
+// console.log(arrClone());
+
+
+// getting first element of array
+
+
+// const getFirst = (input) => {
+//     return input[0]
+// }
+
+// console.log(getFirst([2, 1, 4, 5]));
+// console.log(getFirst(['Bmw', 'Gentra', 'Nexia']));
+
+// console.log(getFirst([[1, 2, 3],2, 1, 4, 5]));
+
+
+
+//getting last element of array
+
+// const getFirst = (input) => {
+//   return input[input.length-1];
+// };
+
+// console.log(getFirst([2, 1, 4, 5]));
+// console.log(getFirst(['Bmw', 'Gentra', 'Nexia']));
+
+// console.log(getFirst([[1, 2, 3], 2, 1, 4, 5]));
+
+//array to string
+
+// const myColor = ['Red', 'Green', 'White', 'Black'];
+
+// // const str = myColor.toString();
+// const str = myColor.join('-')
+// console.log(str)
+
+// sorting numbers
+
+// const arr = [-2, 1, 3, 5, 2, 4, 8, 10];
+
+// const arr1 = arr.sort((a, b) => {
+//     return a - b
+// })
+// console.log(arr1)
+
+
+// uppercase to lowercase
+
+// var str = 'c';
+// var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+// var result = [];
+
+
+// for (let i = 0; )
+
